1ed7760984624fb4770ebebb8abe92ebc181cab1
lucaschaller <luca.schaller@bluewin.ch>
2020-10-14 15:39:22 +0200

    added Product database and add/update/delete/get functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,13 @@ import morgan from 'morgan';
 import { TodoItemController } from './controllers/todoitem.controller';
 import { TodoListController } from './controllers/todolist.controller';
 import { UserController } from './controllers/user.controller';
+import { ProductController } from './controllers/product.controller';
 import { SecuredController } from './controllers/secured.controller';
 import { Sequelize } from 'sequelize';
 import { TodoList } from './models/todolist.model';
 import { TodoItem } from './models/todoitem.model';
 import { User } from './models/user.model';
+import { Product } from './models/product.model';
 
 import cors from 'cors';
 
@@ -25,6 +27,9 @@ export class Server {
         TodoItem.createAssociations();
         TodoList.createAssociations();
         User.initialize(this.sequelize);
+        Product.initialize(this.sequelize);
+        User.createAssociations();
+        Product.createAssociations();
 
         this.sequelize.sync().then(() => {                           // create connection to the database
             this.server.listen(this.port, () => {                                   // start server on specified port
@@ -56,6 +61,7 @@ export class Server {
             .use('/todoitem', TodoItemController)   // any request on this path is forwarded to the TodoItemController
             .use('/todolist', TodoListController)
             .use('/user', UserController)
+            .use('/product', ProductController)
             .use('/secured', SecuredController)
             .options('*', cors(options))
             .use(express.static('./src/public'))
