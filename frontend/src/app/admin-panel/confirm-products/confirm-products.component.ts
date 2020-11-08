2f1dabb40ef8d6d11896f8606ffb8f4943b51e6a
Wing Michelle Vong <wing.vong@students.unibe.ch>
2020-11-08 04:54:44 +0100

    you can now approve and reject (and write a rejection message), but the product cards are synced... need to fix

--- frontend/src/app/admin-panel/confirm-products/confirm-products.component.ts
+++ frontend/src/app/admin-panel/confirm-products/confirm-products.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Product} from '../../models/product.model';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {ProductAttributes} from '../../../../../backend/src/models/product.model';
 
 
 @Component({
@@ -14,6 +13,8 @@ export class ConfirmProductsComponent implements OnInit{
 
   addedProducts: string[] = ['Phone', 'Tomato', 'Rock'];
   products: Product[] = [];
+  rejectField = false;
+  rejectionReason = '';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -25,40 +26,17 @@ export class ConfirmProductsComponent implements OnInit{
   }
 
   approve(product: Product): void {
-    this.httpClient.put(environment.endpointURL + 'product/approve/:' + product.productId, {
-      isApproved: true,
+    this.httpClient.put(environment.endpointURL + 'product/approve/' + product.productId, {
     }).subscribe();
   }
 
   reject(product: Product): void {
-    this.httpClient.put(environment.endpointURL + 'product/reject/:' + product.productId, {
-      isApproved: false,
-      rejectionReason: product.rejectionReason
+    this.httpClient.put(environment.endpointURL + 'product/reject/' + product.productId, {
+      rejectionReason: this.rejectionReason
     }).subscribe();
   }
-
-
+  openReject(): void {
+    this.rejectField = true;
+  }
 }
-/*
-ngOnInit(): void {
-  this.httpClient.get(environment.endpointURL + 'product/uncheckedProducts').subscribe((instances: Product[]) => {
-    this.products = instances.map((instance: ProductAttributes) => {
-      return new Product(instance.productId,
-        instance.isApproved,
-        instance.title,
-        instance.type,
-        instance.price,
-        instance.description,
-        instance.location,
-        instance.isSelling,
-        instance.isAvailable,
-        instance.isDeliverable,
-        instance.rejectionReason,
-        instance.image,
-        instance.review,
-        instance.userId
-      );
-    });
-  });
-}
-*/
+
