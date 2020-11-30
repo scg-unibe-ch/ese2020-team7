49e50ad7c3639b099ee6af70dfe59bdacd26f82e
Wing Michelle Vong <wing.vong@students.unibe.ch>
2020-11-30 19:22:13 +0100

    -

--- frontend/src/app/catalog/buy-product/buy-product.component.ts
+++ frontend/src/app/catalog/buy-product/buy-product.component.ts
@@ -6,6 +6,8 @@ import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {environment} from '../../../environments/environment';
 import {FormBuilder, Validators} from '@angular/forms';
+// import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
 
 
 @Component({
@@ -37,6 +39,7 @@ export class BuyProductComponent implements OnInit {
   zipPattern = '^[0-9]*$';
 
   transactionForm = this.formBuilder.group({
+    returnDate: [null, Validators.required],
     deliveryStreet: ['', Validators.required],
     deliveryPinCode: [null, [Validators.required, Validators.pattern(this.zipPattern)]],
     deliveryCity: ['', Validators.required],
@@ -67,6 +70,14 @@ export class BuyProductComponent implements OnInit {
       if (product.isDeliverable) {
         this.deliver = true;
       }
+      if ((!product.isDeliverable) || (!product.isProduct)) {
+        this.transactionForm.disable({ emitEvent: false });
+      }
+      if (product.isSelling){
+        this.transactionForm.controls.requiredControl.clearValidators();
+        this.transactionForm.updateValueAndValidity();
+      }
+
     });
     this.httpClient.get(environment.endpointURL + 'user/id/' + this.userId, {
     }).subscribe((user: User) => {
@@ -76,32 +87,25 @@ export class BuyProductComponent implements OnInit {
     });
   }
 
-  buyWithAddress(): void {
+  buy(): void {
     this.submitted = true;
+    if (!this.deliver) {
+      this.transactionForm.patchValue({
+        deliveryStreet: null,
+        deliveryPinCode: null,
+        deliveryCity: null,
+        deliveryCountry: null
+      });
+    }
     if (this.transactionForm.invalid) {
       return;
     }
     this.httpClient.post(environment.endpointURL + 'transaction/buy/' + this.productId, {
+      returnDate: this.transactionForm.get('returnDate').value,
       deliveryStreet: this.transactionForm.get('deliveryStreet').value,
       deliveryPinCode: this.transactionForm.get('deliveryPinCode').value,
       deliveryCity: this.transactionForm.get('deliveryCity').value,
       deliveryCountry: this.transactionForm.get('deliveryCountry').value,
-    }).subscribe((res: any) => {
-      console.log(res);
-      this.transaction = res;
-      this.submissionDone = true;
-    }, (error) => {
-      this.transactionError = true;
-      this.errorMessage = JSON.parse(error);
-    });
-  }
-
-  buyWithoutAddress(): void {
-    this.submitted = true;
-    if (this.transactionForm.invalid) {
-      return;
-    }
-    this.httpClient.post(environment.endpointURL + 'transaction/buy/' + this.productId, {
     }).subscribe((res: any) => {
       console.log(res);
       this.transaction = res;
@@ -130,8 +134,15 @@ export class BuyProductComponent implements OnInit {
 
   checkDeliver(): void {
     this.deliver = true;
+    this.transactionForm.enable({ emitEvent: false });
+    this.transactionForm.controls.requiredControl.setValidators([Validators.required]);
+    this.transactionForm.controls.deliveryPincode.setValidators([Validators.pattern(this.zipPattern)]);
+    this.transactionForm.updateValueAndValidity(); // this is to rerun form validation after removing the validation for a field.
   }
   uncheckDeliver(): void {
     this.deliver = false;
+    this.transactionForm.disable({ emitEvent: false });
+    this.transactionForm.controls.requiredControl.clearValidators();
+    this.transactionForm.updateValueAndValidity(); // this is to rerun form validation after removing the validation for a field.
   }
 }
