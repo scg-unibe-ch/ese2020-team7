c950b5e8deb98563618bfa62462dd6e4002eac1d
Lotta Ramseyer <lotta.ramseyer@students.unibe.ch>
2020-12-08 19:44:13 +0100

    width

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {BuyProductComponent} from './catalog/buy-product/buy-product.component';
 import {EditUserComponent} from './registration/edit-user/edit-user.component';
 import {SearchComponent} from './search/search.component';
-import { ForgotPasswordComponent } from './user-login/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -26,8 +25,7 @@ const routes: Routes = [
   { path: 'admin', component: AdminPanelComponent},
   { path: 'buy/:id', component: BuyProductComponent },
   { path: 'settings', component: EditUserComponent },
-  { path: 'search', component: SearchComponent},
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
